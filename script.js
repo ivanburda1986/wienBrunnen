e3dee69470dc5203cb5abd4c7e0efec19590bd19
ivanburda1986 <burda.ivan@gmail.com>
2020-10-14 23:41:33 +0200

    working on the marker clustering

--- script.js
+++ script.js
@@ -8,9 +8,13 @@ let brunnenData;
 const locations = {
   uluru: {lat: -25.344, lng: 131.036},
   prague: {lat: 50.073, lng: 14.418},
+  vienna: {lat: 48.210033, lng: 16.363449},
   kunstwerk: {lat: 16.34422287434822, lng: 48.233598784063105},
   kunstwerk2: {lat: 48.18726563690475, lng: 16.319257220503722},
 }
+
+let techLocations =[];
+let markers = [];
 
 
 //Map
@@ -27,7 +31,8 @@ function initMap(){
 
 function addMarkers(){
   brunnenData.forEach(brunne =>{
-   new google.maps.Marker({position: brunne.coordinates, map: map});
+   new google.maps.Marker({position: brunne.coordinates, label: {text: brunne.name, fontSize: "12px", fontWeight: "500"}, map: map});
+   markers.push(new google.maps.Marker({position: brunne.coordinates, map: map}));
   })
 }
 
@@ -52,9 +57,21 @@ function getBrunnenData(){
         buildIn: brunne.properties.BAUJAHR,
         author: brunne.properties.KUENSTLER
       })
+      techLocations.push([brunne.geometry.coordinates[1], brunne.geometry.coordinates[0]]);
     })
     brunnenData = brunnen;
+    addMarkers();
+    focusLocation(locations.vienna, 10);
   })
 }
 
+
+//Marker clustering
+const imagePath = "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m1.png";
+const markerClusterer = new MarkerClusterer(map, markers, {imagePath: imagePath});
+
+
+(function appInit(){
+  getBrunnenData();
+})();
 
